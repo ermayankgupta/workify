456da429cb839ba6eb512ad869f7697f872be861
mayank gupta <mayank.gupta@bluebash.co>
2024-12-03 00:31:10 +0530

    feat: made the auth with clerk

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import Header from '@/components/header';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
+import { shadesOfPurple } from '@clerk/themes';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,20 +14,37 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
-      <body className={`${inter.className}`}>
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='dark'>
-          <Header />
-          <main className='min-h-screen'>{children}</main>
-          <footer className='bg-gray-900 py-12'>
-            <div className='container mx-auto px-4 text-center text-gray-200'>
-              <p>Made with 💖 by Mayank</p>
-            </div>
-          </footer>
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: shadesOfPurple,
+        variables: {
+          colorPrimary: '#3b82f6',
+          colorBackground: '#1a202c',
+          colorInputBackground: '#2d3748',
+          colorInputText: '#f3f4f6'
+        },
+        elements: {
+          formButtonPrimary: 'text-white',
+          card: 'bg-gray-800',
+          headerTitle: 'text-blue-400',
+          headerSubtitle: 'text-gray-400'
+        }
+      }}>
+      <html lang='en'>
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='dark'>
+            <Header />
+            <main className='min-h-screen'>{children}</main>
+            <footer className='bg-gray-900 py-12'>
+              <div className='container mx-auto px-4 text-center text-gray-200'>
+                <p>Made with 💖 by Mayank</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/header.jsx
+++ components/header.jsx
@@ -1,6 +1,40 @@
+import { SignedIn, SignedOut, SignInButton } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from './ui/button';
+import { PenBox } from 'lucide-react';
+import UserMenu from './user-menu';
 
 export default function Header() {
   return (
-    <div>header</div>
-  )
+    <header className='container mx-auto'>
+      <nav className='px-4 py-6 flex justify-between items-center'>
+        <Link href='/'>
+          <Image
+            src={'/logo2.png'}
+            alt='logo'
+            width={200}
+            height={56}
+            className='h-10 w-auto object-contain'
+          />
+        </Link>
+        <div className='flex items-center gap-4'>
+          <Link href='/project/create'>
+          <Button variant='destructive' className='flex items-center gap-2'>
+            <PenBox size={18}/>
+            <span>Create Project</span>
+          </Button>
+          </Link>
+          <SignedOut>
+            <SignInButton forceRedirectUrl='/onboarding' >
+            <Button variant='outline'>Login</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserMenu/>
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
+  );
 }
